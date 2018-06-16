0a9f0558f007ed0221b268725b9074216b4965aa
Mai <martm0709@hotmail.fr>
2018-06-16 12:44:23 +0000

    work 3

---
+++ app/models/curiosity.rb
@@ -0,0 +1,4 @@
+class Curiosity < ActiveRecord::Base
+     Curiosity.create(name: "Joli mug", description: "Recu au Japon, lors d'un congres interlitieres", image_url: "https://s-media-cache-ak0.pinimg.com/236x/4a/86/bf/4a86bfbf02b472e5b385762b8f267a91.jpg", image_text: "Un grand mug de lait pour bien commencer la journee")
+
+end
