5858e1e9eb9b987d7b54b27dc38ab3f3e709c0c6
Mai <martm0709@hotmail.fr>
2018-06-16 13:26:13 +0000

    week 4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @name = 'Grumpy Cat'
-    @surname = 'Hello!'
-    @description = 'Nice to meet you'
-    @description2 = 'How is it going?'
-    
-    @curiosities = [
-      'pelote de laine',
-      'tapis de souris',
-      'herbe à chat']
+    @my_curiosities = Curiosity.all
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180616110802) do
+ActiveRecord::Schema.define(version: 20180616131519) do
 
   create_table "curiosities", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180616110802) do
     t.string   "image_text"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "category"
   end
 
 end
